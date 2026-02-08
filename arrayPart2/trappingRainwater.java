652f973f8e7f75a0898cceeff1da8d203b21c024
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-08 19:50:05 +0530

    Solved Rain Water Trapping Question

--- arrayPart2/trappingRainwater.java
+++ arrayPart2/trappingRainwater.java
@@ -1,7 +1,35 @@
 package arrayPart2;
 
 public class trappingRainwater {
+
+    public static int rainWaterTrapping(int height[]){
+        int n = height.length;
+
+        // Left Max Boundary :
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        for(int i=1; i<n; i++){
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+
+        // Right Max Boundary :
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for(int i=n-2; i>=0; i--){
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+
+        int trappedWater = 0;
+        for(int i=0; i<n; i++){
+           int waterLevel = Math.min(leftMax[i], rightMax[i]);
+           trappedWater += waterLevel - height[i];
+        }
+
+        return trappedWater;
+    }
     public static void main(String args[]){
-        System.out.println("Rain Water Trapping");
+        int height[] = {4,2,0,6,3,2,5};
+        int TotalWaterTrapped = rainWaterTrapping(height);
+        System.out.println(TotalWaterTrapped);
     }
 }
