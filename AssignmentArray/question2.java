dd2fcf0679c61285900ce36155039de79baa3c2b
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-10 06:35:13 +0530

    Find element in unshorted array

---
+++ AssignmentArray/question2.java
@@ -0,0 +1,18 @@
+package AssignmentArray;
+import java.util.*;
+public class question2 {
+
+    public static int findElement(int nums[], int target){
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int nums[] = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println("Value find at index : " + findElement(nums, target));
+    }
+}
