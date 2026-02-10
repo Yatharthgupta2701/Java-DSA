d18ec4a12885666eb01748c3121fc9ec46f695b9
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-10 05:45:10 +0530

    Best time to buy and sell stock Problem solved

--- arrayPart2/buySellStock.java
+++ arrayPart2/buySellStock.java
@@ -1,7 +1,22 @@
 package arrayPart2;
 import java.util.*;
 public class buySellStock {
+
+    public static int buyAndSellStock(int price[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0; i<price.length; i++){
+            if(buyPrice < price[i]){
+                int profit = price[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = price[i];
+            }
+        }
+        return maxProfit;
+    }
     public static void main(String args[]){
-        System.out.println("Best Time to Buy & Sell Stock");
+        int price[] = {7,1,5,3,6,4};
+        System.out.println(buyAndSellStock(price));
     }
 }
