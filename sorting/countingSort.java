50aec4881b8b1e925896d01120857a51abbe51fa
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-13 08:01:27 +0530

    Counting Sort Done

--- sorting/countingSort.java
+++ sorting/countingSort.java
@@ -10,6 +10,7 @@ public class countingSort {
             largest = Math.max(largest, arr[i]);
         }
 
+        // Store frequency :
         int count[] = new int[largest+1];
         for(int i=0; i<arr.length; i++){
             count[arr[i]]++;
@@ -32,7 +33,7 @@ public class countingSort {
         System.out.println();
     }
     public static void main(String args[]){
-        int arr[] = {5,4,1,3,2};
+        int arr[] = {1,4,1,3,2,4,3,7};
         counting(arr);
     }
 }
