4a924a2531bfb5b6c68ae174653678d617e53f2d
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-16 06:01:41 +0530

    Basic String Lecture

---
+++ strings/stringsBasic.java
@@ -0,0 +1,26 @@
+package strings;
+import java.util.*;
+public class stringsBasic {
+    public static void main(String args[]){
+
+        // // Intilization of Strings :
+        // char arr[] = {'a', 'b', 'c', 'd'};
+        // String str = "abcd";
+
+        // // Input/Outout in Strings :
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+        // name = sc.nextLine();
+        // System.out.println(name);
+
+        // To find length of String :
+        // String fullName = "Yatharth Gupta";
+        // System.out.println(fullName.length());
+
+        // Concatenation :
+        String firstName = "Yatharth";
+        String lastName = "Gupta";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
