996b9b63fbb6ac5a44435c6aac32eaa1a3461fc9
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-18 08:46:02 +0530

    StringBuilder and Uppercase Conversion

---
+++ strings/stringBuilder.java
@@ -0,0 +1,11 @@
+package strings;
+import java.util.*;
+public class stringBuilder {
+    public static void main(String args[]){
+        StringBuilder str = new StringBuilder("");
+        for(char ch='a'; ch<='z'; ch++){
+            str.append(ch);
+        }
+        System.out.println(str);
+    }
+}
