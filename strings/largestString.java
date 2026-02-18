333afc7c01cd6d9700aae9240927e95745d5706f
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-18 07:04:22 +0530

    Largest String : Lexicographically

---
+++ strings/largestString.java
@@ -0,0 +1,14 @@
+package strings;
+import java.util.*;
+public class largestString {
+    public static void main(String args[]){
+        String fruits[] = {"apple", "mango", "banana"};
+        String largest = fruits[0];
+        for(int i=1; i<fruits.length; i++){
+            if(largest.compareTo(fruits[i]) < 0){
+                largest = fruits[i];
+            }
+        }
+        System.out.println("Largest string : " + largest);
+    }
+}
