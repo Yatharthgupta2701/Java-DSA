451e462d29c1df92cde0844ce6daf6b3c732e1c2
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-20 08:04:44 +0530

    Next largest Char

---
+++ strings/findNextLargestLetter.java
@@ -0,0 +1,23 @@
+package strings;
+import java.util.*;
+public class findNextLargestLetter {
+
+    public static char nextGreatestchar(char letters[], char target){
+        int start = 0;
+        int end = letters.length-1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(letters[mid] > target){
+                end = mid-1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+    public static void main(String args[]){
+        char letters[] = {'c','f','j'};
+        char target = 'a';
+        System.out.println(nextGreatestchar(letters, target));
+    }
+}
