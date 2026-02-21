c7c964532d861decea240006abb1e049c34523a5
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-21 20:49:49 +0530

    Peak Element

---
+++ arrayPart1/peakElement.java
@@ -0,0 +1,16 @@
+public class peakElement {
+
+    public static int PeakElement(int nums[]){
+        int peakIndex = 0;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] > nums[peakIndex]){
+                peakIndex = i;
+            }
+        }
+        return peakIndex;
+    }
+    public static void main(String args[]){
+        int nums[] = {1,2,1,3,5,6,4};
+        System.out.println(PeakElement(nums));
+    }
+}
