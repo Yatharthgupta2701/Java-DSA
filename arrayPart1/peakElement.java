294736fd5c89ec680ef535ef341c750d768e1256
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-21 20:51:06 +0530

    Peak Element Problem Solved

--- arrayPart1/peakElement.java
+++ arrayPart1/peakElement.java
@@ -10,7 +10,7 @@ public class peakElement {
         return peakIndex;
     }
     public static void main(String args[]){
-        int nums[] = {1,2,1,3,5,6,4};
+        int nums[] = {1,2,3,1};
         System.out.println(PeakElement(nums));
     }
 }
